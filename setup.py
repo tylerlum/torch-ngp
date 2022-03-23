de622261f86322b8698e07877b60a177d53e84be
Javier Yu <javiercyu@gmail.com>
2022-03-23 09:50:55 -0700

    adding other packages

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 from setuptools import setup
 
-setup(name="torch_ngp", version="0.1", packages=["nerf"])
+setup(name="torch_ngp", version="0.1", packages=["nerf", "sdf", "ffmlp", "shencoder", "raymarching", "hashencoder"])
