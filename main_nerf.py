8c631742c0e93f0b5721277defdac815ce2cd6d8
Tyler Lum <tylergwlum@gmail.com>
2023-04-13 17:07:39 -0700

    Remove code redundancy

--- main_nerf.py
+++ main_nerf.py
@@ -35,27 +35,11 @@ if __name__ == '__main__':
 
     # test mode
     if opt.test:
-
         trainer = Trainer('ngp', vars(opt), model, workspace=opt.workspace, criterion=criterion,
                           fp16=opt.fp16, metrics=[PSNRMeter()], use_checkpoint='latest')
 
-        # Gui vs. cmd mode
-        if opt.gui:
-            gui = NeRFGUI(opt, trainer)
-            gui.render()
-        else:
-            test_dataset = NeRFDataset(opt.path, type='test', mode=opt.mode, scale=opt.scale,
-                                       preload=opt.preload, trans_noise=opt.trans_noise, rot_noise=opt.rot_noise)
-            test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=1)
-
-            if opt.mode == 'blender':
-                trainer.evaluate(test_loader)  # blender has gt, so evaluate it.
-            else:
-                trainer.test(test_loader)  # colmap doesn't have gt, so just test.
-
     # train mode
     else:
-
         def optimizer(model):
             return torch.optim.Adam(
                 params=[
@@ -82,6 +66,7 @@ if __name__ == '__main__':
                 gamma=0.33
             )
 
+        # Setup training
         trainer = Trainer('ngp', vars(opt), model, workspace=opt.workspace, optimizer=optimizer, criterion=criterion, ema_decay=0.95,
                           fp16=opt.fp16, lr_scheduler=scheduler, metrics=[PSNRMeter()], use_checkpoint='latest', eval_interval=50)
 
@@ -99,29 +84,32 @@ if __name__ == '__main__':
             trainer.W = train_dataset.W
             trainer.pose_scheduler = optim.lr_scheduler.MultiStepLR(pose_optimizer, milestones=[50, 100, 150], gamma=1.) 
 
-        if opt.gui:  # GUI MODE
-
+        # GUI MODE
+        if opt.gui:
             # attach dataloader to trainer
             trainer.train_loader = train_loader
             # very slow, can take ~10s to shuffle...
             trainer.loader = iter(train_loader)
 
-            # Create pose optimizer + attach, if training.
-            gui = NeRFGUI(opt, trainer)
-            gui.render()
-
-        else:  # CMD MODE
-
+        # CMD MODE
+        else:
             valid_dataset = NeRFDataset(opt.path, type='val', mode=opt.mode, downscale=2, scale=opt.scale, preload=opt.preload)
             valid_loader = torch.utils.data.DataLoader(valid_dataset, batch_size=1, pin_memory=True)
 
             # train 200 epochs, each epoch has 100 steps --> in total 20,000 steps
             trainer.train(train_loader=train_loader, valid_loader=valid_loader, max_epochs=200, max_steps_per_epoch=100)
 
-            # also test
-            test_dataset = NeRFDataset(opt.path, type='test', mode=opt.mode, scale=opt.scale, preload=opt.preload)
-            test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=1, pin_memory=True)
-            if opt.mode == 'blender':
-                trainer.evaluate(test_loader)  # blender has gt, so evaluate it.
-            else:
-                trainer.test(test_loader)  # colmap doesn't have gt, so just test.
+    # Gui vs. cmd mode
+    if opt.gui:
+        gui = NeRFGUI(opt, trainer)
+        gui.render()
+    else:
+        test_dataset = NeRFDataset(opt.path, type='test', mode=opt.mode, scale=opt.scale,
+                                   preload=opt.preload, trans_noise=opt.trans_noise, rot_noise=opt.rot_noise)
+        test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=1)
+
+        if opt.mode == 'blender':
+            trainer.evaluate(test_loader)  # blender has gt, so evaluate it.
+        else:
+            trainer.test(test_loader)  # colmap doesn't have gt, so just test.
+
